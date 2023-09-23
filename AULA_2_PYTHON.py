0160b48ef9746ec7eb785cebe67382ef621fabd1
ricardosamm <145858657+ricardosamm@users.noreply.github.com>
2023-09-23 17:19:22 -0300

    Add files via upload

---
+++ AULA_2_PYTHON.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um número \n"))
+numero2 = int(input("Digite um número \n"))
+numero3 = numero + numero2
+print(numero3)
+nome = input("Qual seu nome? \n")
+print("olá você esta bem", nome)
+numero = int(input("Digite um número \n"))
+numero2 = int(input("Digite um número \n"))
+numero3 = numero * numero2
+print(numero3)
